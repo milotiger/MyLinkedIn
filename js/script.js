9fbef7ae81f91cd88893c14de440d3daefc671d1
Thanh Trí Nguyễn <Thanh Tr Nguyn>
2016-04-27 16:56:28 +0700

    Json binding

--- js/script.js
+++ js/script.js
@@ -2,13 +2,25 @@
 
 var app = angular.module('linkedinapp', ['ngSanitize']);
 
-app.controller('overviewctrl', function ($scope, $http) {
+app.controller('maincontroller', function ($scope, $http) {
     window.SC = $scope;
     
     $http.get('data/overview.json')
         .then(function(res)
         {
             $scope.overview = res.data;
-        })
+        });
+        
+    $http.get('data/background.json')
+        .then(function(res2)
+        {
+            $scope.background = res2.data;
+        });    
+        
+    $scope.contactvisibility = true;
+    
+    $scope.showhide = function showhide() {
+        $scope.contactvisibility = !$scope.contactvisibility;
+    }
 });
 
