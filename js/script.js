f107095905412b04937f034cb0f681e7f16fa032
Thanh Trí Nguyễn <Thanh Tr Nguyn>
2016-05-03 22:14:47 +0700

    add some code

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 /// <reference path="C:\Users\ADMIN\Dropbox\Tài Liệu\Năm III\HKII\LTWeb\Ref\js\_all.ts" />
 
-var app = angular.module('linkedinapp', ['ngSanitize', 'ngAnimate']);
+var app = angular.module('linkedinapp', ['ngSanitize']);
+
+app.directive('repeatDone', function() {
+    return function(scope, element, attrs) {
+        if (scope.$last) { // all are rendered
+            scope.$eval(attrs.repeatDone);
+        }
+    }
+});
 
 app.controller('maincontroller', function ($scope, $http) {
     window.SC = $scope;
@@ -19,6 +27,39 @@ app.controller('maincontroller', function ($scope, $http) {
         
     $scope.contactvisibility = true;
     $scope.overvieweditvisibility = false;
+    $scope.summaryeditvisibility = false;
+    
+    $scope.update_overview = function updateoverview() {
+        if ($scope.new_overview.name != undefined)
+            $scope.overview.name = $scope.new_overview.name;
+        if ($scope.new_overview.headline != undefined)
+            $scope.overview.headline = $scope.new_overview.headline;
+        if ($scope.new_overview.role != undefined)
+            $scope.overview.role = $scope.new_overview.role;
+        if ($scope.new_overview.country != undefined)
+            $scope.overview.country = $scope.new_overview.country;
+        if ($scope.new_overview.previous != undefined)
+            $scope.overview.previous = $scope.new_overview.previous;
+        if ($scope.new_overview.education != undefined)
+            $scope.overview.education = $scope.new_overview.education;
+        if ($scope.new_overview.faculty != undefined)
+            $scope.overview.faculty = $scope.new_overview.faculty;
+        if ($scope.new_overview.email != undefined)
+            $scope.overview.email = $scope.new_overview.email;
+        if ($scope.new_overview.phone != undefined)
+            $scope.overview.phone = $scope.new_overview.phone;
+        if ($scope.new_overview.link != undefined)
+            $scope.overview.link = $scope.new_overview.link;
+        $scope.new_overview = undefined;
+    }
+    
+    $scope.initModals = function() {
+    $('.modal-trigger').leanModal(); // Initialize the modals
+    }
    
+});
+
+$(document).ready(function() {
+  $('.modal-trigger').leanModal();
 });
 
